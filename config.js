84cc5f216d35869fc2201869c4e7bca35a6ce6fd
Iure <32586599+iure11@users.noreply.github.com>
2026-07-31 19:26:02 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const CONFIG = {
   recipientName: "Júlia",
   senderName: "Iure",
   invitationId: "convite-julia-2026",
-  googleScriptUrl: "COLOCAR_URL_DO_APPS_SCRIPT_AQUI",
-  whatsappNumber: "COLOCAR_NUMERO_AQUI"
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbx7nqD7bvwd-9zKZoodAFVyYV0yP6CNxHl7RGVw_efsDWVDez2ljuIdekpI7TPyyRk8/exec",
+  whatsappNumber: "34999110991"
 };
